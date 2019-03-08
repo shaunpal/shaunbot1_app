c9e2abc533ef6412898dde65cfd0ee66d033fad1
Shaun <shaun@Shauns-MacBook-Pro.local>
2019-03-08 11:26:16 +0800

    inline example

--- index.js
+++ index.js
@@ -12,20 +12,27 @@ bot.help(({ reply }) => reply("Hello I'm the Solex Bot"));
 
 const coffeeOrderSessions = [];
 
-bot.command("orderopen", ctx => {
-  const { from } = ctx.update.message;
-  const coffeeGif = "CgADBAADdwADg731Uoi1wlQtUhaEAg";
-  ctx.replyWithAnimation(coffeeGif, { caption: "Coffee Order Open!" });
-  const newSession = {
-    from,
-    orders: [],
-    isOrderOpen: true
-  };
-  coffeeOrderSessions.push(newSession);
-});
+// bot.command("orderopen", ctx => {
+//   const { from } = ctx.update.message;
+//   const coffeeGif = "CgADBAADdwADg731Uoi1wlQtUhaEAg";
+//   ctx.replyWithAnimation(coffeeGif, { caption: "Coffee Order Open!" });
+//   const newSession = {
+//     from,
+//     orders: [],
+//     isOrderOpen: true
+//   };
+//   coffeeOrderSessions.push(newSession);
+// });
 
 bot.start(ctx => ctx.reply("Hello"));
 bot.command("hello", context => context.reply("hello to you too!!"));
+bot.command('inline', (ctx) => {
+  return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
+    m.inlineKeyboard([
+      m.callbackButton('Coke', 'Coke'),
+      m.callbackButton('Pepsi', 'Pepsi')
+    ])))
+})
 if (process.env.NODE_ENV === "production") {
   bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
   bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
